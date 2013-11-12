3486c2df8ef2e6a6af2815eb5c59709560eb4b1c
Mihnea Simian <contact@mesimian.com>
2013-11-12 17:51:05 +0200

    jslint fixes refs #17382

--- eea/workflow/skins/eea_workflow_templates/archive_dialog.js
+++ eea/workflow/skins/eea_workflow_templates/archive_dialog.js
@@ -1,3 +1,5 @@
+/*globals $, window, document, context_url */
+
 function ArchiveDialog() {
     this.install();
 }
@@ -24,10 +26,12 @@ ArchiveDialog.prototype.install = function () {
     is_minimal = $state_menu.find("dd").length;
     if (is_minimal === 0) {
         $("#contentActionMenus").prepend(
-                $("<dl>").addClass("actionMenu deactivated").append(
-                    $("<dt>").addClass("actionMenuHeader").append(
-                        $("<span>").append($archive_action))
-                ));
+            $("<dl>").addClass("actionMenu deactivated").append(
+                $("<dt>").addClass("actionMenuHeader").append(
+                    $("<span>").append($archive_action)
+                )
+            )
+        );
     } else {
         $advanced = $("#workflow-transition-advanced").parent();
         if ($advanced.length === 0) {
@@ -48,9 +52,9 @@ ArchiveDialog.prototype.install = function () {
 ArchiveDialog.prototype.onclick_archive = function (self, e) {
     // this is a partial function, it curries the self object
     // it is needed because jquery event object are detached from the OOP object
-    if (typeof(e) === "undefined") {
-        return function (e) {
-            self.open_dialog(self, "archive");
+    if (e === undefined) {
+        return function () {
+            self.open_dialog("archive");
             return false;
         };
     }
@@ -59,30 +63,30 @@ ArchiveDialog.prototype.onclick_archive = function (self, e) {
 ArchiveDialog.prototype.onclick_unarchive = function (self, e) {
     // this is a partial function, it curries the self object
     // it is needed because jquery event object are detached from the OOP object
-    if (typeof(e) === "undefined") {
-        return function (e){
-            self.open_dialog(self, "unarchive");
+    if (e === undefined) {
+        return function () {
+            self.open_dialog("unarchive");
             return false;
         };
     }
 };
 
-ArchiveDialog.prototype.open_dialog = function (self, action_type){
+ArchiveDialog.prototype.open_dialog = function (action_type) {
     var w = new ArchiveDialog.Window();
-    switch(action_type){
-        case "archive":
-            w.open_archive();
-            break;
-        case "unarchive":
-            w.open_unarchive();
-            break;
+    switch (action_type) {
+    case "archive":
+        w.open_archive();
+        break;
+    case "unarchive":
+        w.open_unarchive();
+        break;
     }
 };
 
 ArchiveDialog.Window = function () {
-    var $target = jQuery("#archive-dialog-target");
-    if ($target.length === 0){
-        $target = jQuery("<div>").appendTo("body").attr('id', 'archive-dialog-target');
+    var $target = $("#archive-dialog-target");
+    if ($target.length === 0) {
+        $target = $("<div>").appendTo("body").attr('id', 'archive-dialog-target');
     }
     this.target = $target;
 };
@@ -90,7 +94,7 @@ ArchiveDialog.Window = function () {
 ArchiveDialog.Window.prototype.open_archive = function () {
     $("dl.activated").removeClass('activated');
     var self = this;
-    self.dialog = jQuery(this.target).dialog({
+    self.dialog = $(this.target).dialog({
         title: "Expire/Archive content",
         dialogClass: 'archiveDialog',
         modal: true,
@@ -100,16 +104,16 @@ ArchiveDialog.Window.prototype.open_archive = function () {
         open: function (ui) { self._open_archive(ui); },
         buttons: {
             'Ok': function (e) { self.handle_ok_archive(e); },
-        'Cancel': function (e) { self.handle_cancel(e); }
+            'Cancel': function (e) { self.handle_cancel(e); }
         }
-    }
-    );
+        }
+        );
 };
 
 ArchiveDialog.Window.prototype.open_unarchive = function () {
     $("dl.activated").removeClass('activated');
     var self = this;
-    self.dialog = jQuery(this.target).dialog({
+    self.dialog = $(this.target).dialog({
         title: "UnArchive content",
         dialogClass: 'archiveDialog',
         modal: true,
@@ -119,53 +123,52 @@ ArchiveDialog.Window.prototype.open_unarchive = function () {
         open: function (ui) { self._open_unarchive(ui); },
         buttons: {
             'Ok': function (e) { self.handle_ok_unarchive(e); },
-        'Cancel': function (e) { self.handle_cancel(e); }
+            'Cancel': function (e) { self.handle_cancel(e); }
         }
-    }
-    );
+        }
+        );
 };
 
-ArchiveDialog.Window.prototype.handle_cancel = function (e) {
+ArchiveDialog.Window.prototype.handle_cancel = function () {
     this.dialog.dialog("close");
 };
 
-ArchiveDialog.Window.prototype.handle_ok_archive = function (e) {
+ArchiveDialog.Window.prototype.handle_ok_archive = function () {
     var self = this,
         $form,
-        workflow_reason = jQuery("input[name='workflow_reasons_radio']:checked").val(),
-        hasErrors = false,
-        preloader;
-    jQuery('.notice').remove();
+        workflow_reason = $("input[name='workflow_reasons_radio']:checked").val(),
+        hasErrors = false;
+    $('.notice').remove();
     if (!workflow_reason) {
-        jQuery("#workflow_reason_label").after("<div class='notice' style='color:Black; background-color:#FFE291; " +
+        $("#workflow_reason_label").after("<div class='notice' style='color:Black; background-color:#FFE291; " +
                 "padding:3px'>Please select reason</div>");
         hasErrors = true;
     }
-    if ((workflow_reason === 'other') && (!jQuery("input[name='workflow_other_reason']").val())) {
-        jQuery("input[name='workflow_other_reason']").after("<div class='notice' style='color:Black; background-color:#FFE291; " +
+    if ((workflow_reason === 'other') && (!$("input[name='workflow_other_reason']").val())) {
+        $("input[name='workflow_other_reason']").after("<div class='notice' style='color:Black; background-color:#FFE291; " +
                 "padding:3px'>Please specify reason</div>");
         hasErrors = true;
     }
 
-    if (!jQuery("input[name='workflow_archive_initiator']").val()) {
-        jQuery("#workflow_initiator_label").after("<div class='notice' style='color:Black; background-color:#FFE291; " +
+    if (!$("input[name='workflow_archive_initiator']").val()) {
+        $("#workflow_initiator_label").after("<div class='notice' style='color:Black; background-color:#FFE291; " +
                 "padding:3px'>Please specify initiator</div>");
         hasErrors = true;
     }
     if (hasErrors) {
-        jQuery(".notice").effect("pulsate", {times: 3}, 2000,
-                function () { jQuery('.notice').remove(); });
+        $(".notice").effect("pulsate", {times: 3}, 2000,
+                function () { $('.notice').remove(); });
         return;
     }
     $("#archiving_preloader").show();
-    
-    $form = jQuery("form", this.target);
-    $.post($form.attr('action'), $form.serialize(), function (res) {
+
+    $form = $("form", this.target);
+    $.post($form.attr('action'), $form.serialize(), function () {
         $('.archive_status').remove();
         self.dialog.dialog("close");
         $("#workflow-transition-archive").remove();
         $("#archiving_preloader").hide();
-        $.get(context_url+"/@@eea.workflow.archived", function (dom) {
+        $.get(context_url + "/@@eea.workflow.archived", function (dom) {
             $("#plone-document-byline").after(dom);
             window.object_archived = true;
             new ArchiveDialog();
@@ -175,11 +178,11 @@ ArchiveDialog.Window.prototype.handle_ok_archive = function (e) {
     return false;
 };
 
-ArchiveDialog.Window.prototype.handle_ok_unarchive = function (e) {
+ArchiveDialog.Window.prototype.handle_ok_unarchive = function () {
     var self = this,
-        $form = jQuery("form", this.target);
+        $form = $("form", this.target);
     $("#unarchiving_preloader").show();
-    $.post($form.attr('action'), $form.serialize(), function (res) {
+    $.post($form.attr('action'), $form.serialize(), function () {
         $("#unarchiving_preloader").remove();
         self.dialog.dialog("close");
         $("#workflow-transition-unarchive").remove();
@@ -191,29 +194,29 @@ ArchiveDialog.Window.prototype.handle_ok_unarchive = function (e) {
     return false;
 };
 
-function get_base(){
-    var base = (window.context_url || jQuery("base").attr('href') || document.baseURI ||
+function get_base() {
+    var base = (window.context_url || $("base").attr('href') || document.baseURI ||
                 window.location.href.split("?")[0].split('@@')[0]);
     return base;
 }
 
-ArchiveDialog.Window.prototype._open_archive = function (ui) {
+ArchiveDialog.Window.prototype._open_archive = function () {
     var self = this,
         base = get_base(),
         url = base + "/archive_dialog";
 
-    jQuery(self.target).load(url, function () {});
+    $(self.target).load(url);
 };
 
-ArchiveDialog.Window.prototype._open_unarchive = function (ui) {
+ArchiveDialog.Window.prototype._open_unarchive = function () {
     var self = this,
         base = get_base(),
         url = base + "/unarchive_dialog";
 
-    jQuery(self.target).load(url, function () {});
+    $(self.target).load(url);
 };
 
-jQuery(document).ready(function ($) {
+$(document).ready(function () {
     var ad = new ArchiveDialog();
 });
 
