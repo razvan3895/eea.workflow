5ff3f0f3ce20a30d83225c18a09f78d164262900
ichim-david <ichim.david@gmail.com>
2014-02-27 17:45:19 +0200

    Refs #18316; implemented logic that applies the archive and unarchive actions recursive to it's children if the boolean flag is set within the content rule to perform the tasks recursively

--- eea/workflow/rules/actions.py
+++ eea/workflow/rules/actions.py
@@ -9,6 +9,8 @@ from OFS.SimpleItem import SimpleItem
 from plone.contentrules.rule.interfaces import IExecutable, IRuleElementData
 from plone.app.contentrules.browser.formhelper import AddForm as PloneAddForm
 from plone.app.contentrules.browser.formhelper import EditForm as PloneEditForm
+from Products.CMFPlone.utils import getToolByName
+
 from eea.workflow.rules.interfaces import IArchiveUnarchiveAction
 from eea.workflow.interfaces import IObjectArchivator
 from eea.versions.interfaces import IGetVersions
@@ -60,23 +62,44 @@ class ArchiveUnarchiveExecutor(object):
         orig_obj_url = obj.absolute_url(1)
 
         adapter = IObjectArchivator(obj)
+        val = dict(initiator='contentRules',
+                   reason='Other', custom_message='new version %s'
+                                                  ' was published' %
+                                                  orig_obj_url)
         if action == "archived":
             if self.element.affectPreviousVersion:
                 obj = IGetVersions(obj).versions()[-2]
                 adapter = IObjectArchivator(obj)
-            adapter.archive(obj, **dict(initiator='contentRules',
-                            reason='Other', custom_message='new version '
-                                                           'published'))
+            if self.element.applyRecursively:
+                self.recursive_action(obj, 'archive', val)
+            else:
+                adapter.archive(obj, **val)
         else:
+            val = dict(custom_message='Unarchived by content'
+                                      ' rule because latest version %s'
+                                      ' was unpublished' % (orig_obj_url))
             if self.element.affectPreviousVersion:
                 obj = IGetVersions(obj).versions()[-2]
                 adapter = IObjectArchivator(obj)
-            adapter.unarchive(obj, custom_message='Unarchived by content rule'
-                                       ' because latest version %s'
-                                       ' was unpublished' % (orig_obj_url))
+            if self.element.applyRecursively:
+                self.recursive_action(obj, 'unarchive', val)
+            else:
+                adapter.unarchive(obj, **val)
         logger.info("Object %s state is %s", obj.absolute_url(),
                     action)
         return True
+
+    def recursive_action(self, orig_obj, action, val):
+        """
+        """
+        catalog = getToolByName(self.context, 'portal_catalog')
+        query = {'path': '/'.join(orig_obj.getPhysicalPath())}
+        brains = catalog.searchResults(query)
+        for brain in brains:
+            obj = brain.getObject()
+            storage = IObjectArchivator(obj)
+            getattr(storage, action)(obj, **val)
+
 
 
 class AddForm(PloneAddForm):
